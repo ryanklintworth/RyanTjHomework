a8f5440a2830c5a347e64780aa6fdd08500e59f2
ryanklintworth <ryan.klintworth@gmail.com>
2021-01-13 17:58:43 -0600

    render has a div with a form inside of it

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,41 @@ class App extends React.Compontent {
     rated:'',
     movies:[]
   }
-  
+
+  render = () => {
+    return(
+      <div>
+        <h1>TITLE</h1>
+        <form obSubmit={this.handleSubmit}>
+          <label htmlFor="name">Name</label><br/>
+          <input
+            type="text"
+            id="name"
+            onChange={this.handleChange}
+            value={this.state.image}/><br/>
+          <label htmlFor="name">Image</label><br/>
+          <input
+            type="text"
+            id="name"
+            onChange={this.handleChange}
+            value={this.state.image}/><br/>
+          <label htmlFor="name">Date</label><br/>
+          <input
+            type="text"
+            id="name"
+            onChange={this.handleChange}
+            value={this.state.date}/><br/>
+          <label htmlFor="name">Rated</label><br/>
+          <input
+            type="text"
+            id="name"
+            onChange={this.handleChange}
+            value={this.state.rated}/><br/>
+          <input type="submit"/>
+        </form>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App></App>, document.querySelector('main'))

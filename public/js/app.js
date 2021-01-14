6c17735e45ea4a9b573c52c0f731a5d25aa5bfec
Tejiri Ibihwiori <tejiri@Tejiris-MacBook-Pro.local>
2021-01-13 21:26:55 -0600

    haha

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,5 @@
 class App extends React.Compontent {
-  state = {
-    name:'',
-    image:'',
-    date:'',
-    rated:'',
-    movies:[]
-  }
-  
+
 }
 
 ReactDOM.render(<App></App>, document.querySelector('main'))
